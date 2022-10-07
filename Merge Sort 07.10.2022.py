7d9c6cc3e86cba6374e0d5c2797948f519ab4aed
CezarAmarandei <115228145+CezarAmarandei@users.noreply.github.com>
2022-10-07 23:21:55 +0300

    Update Merge Sort 07.10.2022.py

--- Merge Sort 07.10.2022.py
+++ Merge Sort 07.10.2022.py
@@ -1,21 +1,36 @@
-def partition(array, Primul, Ultimul):
-  pivot = array[Ultimul] # Alegem pivotul ca ultim element
-  i = Primul - 1 # pointerul
-  for j in range(Primul, Ultimul): #parcurgem toata lista comparand-o cu pivotul
-    if array[j] <= pivot: # daca elementul este mai mic sau egal cu pivotul, il mutam in stanga
-      i = i + 1
-      (array[i], array[j]) = (array[j], array[i]) # interschimbam elementele
-  (array[i + 1], array[Ultimul]) = (array[Ultimul], array[i + 1]) # interschimbam pivotul cu elementul din dreapta
-  return i + 1 # returnam pozitia pivotului
+def mergeSort(array): # functia de sortare
+    if len(array) > 1: # daca lungimea array-ului este mai mare decat 1, adica inca nu am impartit lista in cate un element
+        r = len(array)//2 #  r este mijlocul listei
+        L = array[:r] # L este lista din stanga
+        M = array[r:] #M este lista din dreapta
+        mergeSort(L) #sortam lista din stanga
+        mergeSort(M) #sortam lista din dreapta
+        i = j = k = 0 # i,j,k sunt indexii ce pornesc de la zero
+        while i < len(L) and j < len(M): # cat timp i si j sunt mai mici decat lungimile listelor
+            if L[i] < M[j]: # daca elementul din lista din stanga este mai mic decat cel din dreapta
+                array[k] = L[i] # daca da, atunci elementul din lista din stanga va fi pus in array
+                i += 1 # se incrementeaza i
+            else:
+                array[k] = M[j] # daca nu, atunci elementul din lista din dreapta va fi pus in array
+                j += 1 # se incrementeaza j
+            k += 1 # 
+        while i < len(L): #daca ramanem fara elemente in lista din stanga
+            array[k] = L[i] # atunci elementele din lista din stanga vor fi puse in array
+            i += 1
+            k += 1
 
-# Functia ce efectueaza QuickSort
-def quickSort(array, Primul, Ultimul):
-  if Primul < Ultimul:
-    pi = partition(array, Primul, Ultimul)
-    quickSort(array, Primul, pi - 1) # apelam functia pentru partea stanga
-    quickSort(array, pi + 1, Ultimul) # apelam functia pentru partea dreapta
+        while j < len(M): #daca ramanem fara elemente in lista din dreapta
+            array[k] = M[j] # atunci elementele din lista din dreapta vor fi puse in array
+            j += 1
+            k += 1
 
-Lista = [8, 7, 2, 1, 0, 9, 6, 5, 4, 3, 12, 99, 1, 5]
-print("Lista nesortata: ", Lista)
-quickSort(Lista, 0, len(Lista) - 1)
-print('Lista sortata in ordine crescatoare: ', Lista)
+def printList(array): # functia de printare
+    for i in range(len(array)): # pentru fiecare element din array
+        print(array[i], end=" ") # printam elementul urmat de un spatiu
+    print() # printam un rand nou
+
+if __name__ == '__main__': # daca fisierul este rulat direct
+    array = [6, 5, 12, 10, 9, 1]
+    mergeSort(array) # sortam array-ul
+    print("Sorted array is: ") 
+    printList(array) # printam array-ul sortat
