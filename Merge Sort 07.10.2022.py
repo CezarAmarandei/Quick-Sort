f0a9eb8e3eb51d915a04f8b6067d7ab4f72f17c6
CezarAmarandei <115228145+CezarAmarandei@users.noreply.github.com>
2022-10-07 22:10:47 +0300

    Update Merge Sort 07.10.2022.py

--- Merge Sort 07.10.2022.py
+++ Merge Sort 07.10.2022.py
@@ -1,51 +1,21 @@
-def partition(array, low, high):
-
-  # choose the rightmost element as pivot
-  pivot = array[high]
-
-  # pointer for greater element
-  i = low - 1
-
-  # traverse through all elements
-  # compare each element with pivot
-  for j in range(low, high):
-    if array[j] <= pivot:
-      # if element smaller than pivot is found
-      # swap it with the greater element pointed by i
+def partition(array, Primul, Ultimul):
+  pivot = array[Ultimul] # Alegem pivotul ca ultim element
+  i = Primul - 1 # pointerul
+  for j in range(Primul, Ultimul): #parcurgem toata lista comparand-o cu pivotul
+    if array[j] <= pivot: # daca elementul este mai mic sau egal cu pivotul, il mutam in stanga
       i = i + 1
+      (array[i], array[j]) = (array[j], array[i]) # interschimbam elementele
+  (array[i + 1], array[Ultimul]) = (array[Ultimul], array[i + 1]) # interschimbam pivotul cu elementul din dreapta
+  return i + 1 # returnam pozitia pivotului
 
-      # swapping element at i with element at j
-      (array[i], array[j]) = (array[j], array[i])
+# Functia ce efectueaza QuickSort
+def quickSort(array, Primul, Ultimul):
+  if Primul < Ultimul:
+    pi = partition(array, Primul, Ultimul)
+    quickSort(array, Primul, pi - 1) # apelam functia pentru partea stanga
+    quickSort(array, pi + 1, Ultimul) # apelam functia pentru partea dreapta
 
-  # swap the pivot element with the greater element specified by i
-  (array[i + 1], array[high]) = (array[high], array[i + 1])
-
-  # return the position from where partition is done
-  return i + 1
-
-# function to perform quicksort
-def quickSort(array, low, high):
-  if low < high:
-
-    # find pivot element such that
-    # element smaller than pivot are on the left
-    # element greater than pivot are on the right
-    pi = partition(array, low, high)
-
-    # recursive call on the left of pivot
-    quickSort(array, low, pi - 1)
-
-    # recursive call on the right of pivot
-    quickSort(array, pi + 1, high)
-
-
-data = [8, 7, 2, 1, 0, 9, 6]
-print("Unsorted Array")
-print(data)
-
-size = len(data)
-
-quickSort(data, 0, size - 1)
-
-print('Sorted Array in Ascending Order:')
-print(data)
+Lista = [8, 7, 2, 1, 0, 9, 6, 5, 4, 3, 12, 99, 1, 5]
+print("Lista nesortata: ", Lista)
+quickSort(Lista, 0, len(Lista) - 1)
+print('Lista sortata in ordine crescatoare: ', Lista)
