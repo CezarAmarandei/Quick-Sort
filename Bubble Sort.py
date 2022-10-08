6861e075b76e8fbc7d58425fb9001ed56c5b6470
CezarAmarandei <115228145+CezarAmarandei@users.noreply.github.com>
2022-10-08 16:37:36 +0300

    Update Bubble Sort.py

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,11 +1,11 @@
 def bubbleSort(array):          # functia bubbleSort
-  for i in range(len(array)):           # parcurgem array-ul
-    for j in range(0, len(array) - i - 1):   # parcurgem array-ul   
-      if array[j] > array[j + 1]:    # daca elementul curent este mai mare decat urmatorul  
-        temp = array[j]     # interschimbam elementelea
-        array[j] = array[j+1]       # interschimbam elementelea
-        array[j+1] = temp       # interschimbam elementeleaa
-data = [-2, 45, 0, 11, -9]      # vectorul-ul
+  for i in range(len(array)):           # parcurgem vectorul de la 0 la lungimea vectorului
+    for j in range(0, len(array) - i - 1):   # parcurgem vectorul de la 0 la lungimea vectorului - i - 1
+      if array[j] > array[j + 1]:    # daca elementul curent este mai mare decat urmatorul, interschimbam elementelea
+        temp = array[j]        # salvam elementul curent intr-o variabila temporara
+        array[j] = array[j+1]
+        array[j+1] = temp
 
+data = [-2, 45, 0, 11, -9]      # vectorul-ul de sortat
 bubbleSort(data)        # apelam functia bubbleSort
 print('Vectorul sortat in ordine crescatoare: ', data)       # afisam vectorul sortat
